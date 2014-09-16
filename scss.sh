80afd6fd2654ee50324fb22ac18ae277c179abbf
Brandon Martinez <me@brandonmartinez.com>
2014-09-15 23:51:08 -0400

    Moving all the mockup stuff into it's own dir for development. Ripped out other SCSS

--- scss.sh
+++ scss.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 
-scss --no-cache --watch src/files/scss/style.scss:src/files/style.css  --style compressed 
+#scss --no-cache --watch src/files/scss/style.scss:src/files/style.css  --style compressed 
+scss --no-cache --watch mockup/files/scss/:mockup/  --style compressed 
 
 wait
