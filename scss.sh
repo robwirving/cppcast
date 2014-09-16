8c47b2d7a8c8ec1d89958df741c8db0216fe89b4
Brandon Martinez <me@brandonmartinez.com>
2014-09-16 00:17:31 -0400

    Basic header structure started

--- scss.sh
+++ scss.sh
@@ -2,6 +2,6 @@
 set -x
 
 #scss --no-cache --watch src/files/scss/style.scss:src/files/style.css  --style compressed 
-scss --no-cache --watch mockup/files/scss/:mockup/  --style compressed 
+scss --no-cache --watch mockup/files/scss/:mockup/styles/  --style compressed 
 
 wait
