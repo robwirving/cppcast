52f16ea3fbc341f2e03740eb4715142839cd2ddc
Brandon Martinez <me@brandonmartinez.com>
2014-10-02 13:01:09 -0400

    Fixed audio control bug

--- src/files/scripts/site.audioplayer.js
+++ src/files/scripts/site.audioplayer.js
@@ -1,5 +1,17 @@
 $(function(){
-	if(!audioPlayer){
-		$('#audioplayer').audioPlayer();
+	var $audioPlayer = $('#audioplayer');
+	if($audioPlayer.length) {
+		var audioTag = document.createElement('audio');
+
+	    /* Do we not support MP3 audio? If not, dynamically made a Flash SWF player.  */
+	    if (!!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''))) {
+	        AudioPlayer.embed("audioplayer_1", {soundFile: "<%= @audioUrl %>", transparentpagebg: "yes"});
+	        $audioPlayer.hide();
+	    }
+	    else /* Ok, we do support MP3 audio, style the audio tag into a touch-friendly player */
+	    {
+	        /* If we didn't do the "if mp3 supported" check above, this call would prompt Firefox install quicktime! */
+	        $audioPlayer.audioPlayer();
+	    }
 	}
 });
