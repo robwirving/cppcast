ec4f997decf8e15635bd17b0c495feb0a4678a03
Brandon Martinez <me@brandonmartinez.com>
2014-10-02 13:06:23 -0400

    a proper fix. Also, firefox is ugly because no mp3 support. Something to look into

--- src/files/scripts/site.audioplayer.js
+++ src/files/scripts/site.audioplayer.js
@@ -1,12 +1,11 @@
 $(function(){
 	var $audioPlayer = $('#audioplayer');
 	if($audioPlayer.length) {
-		var audioTag = document.createElement('audio');
+		var audiotag = document.createElement('audio');
 
 	    /* Do we not support MP3 audio? If not, dynamically made a Flash SWF player.  */
-	    if (!!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''))) {
-	        AudioPlayer.embed("audioplayer_1", {soundFile: "<%= @audioUrl %>", transparentpagebg: "yes"});
-	        $audioPlayer.hide();
+	    if (!!(audiotag.canPlayType && audiotag.canPlayType('audio/mpeg;').replace(/no/, ''))) {
+	        
 	    }
 	    else /* Ok, we do support MP3 audio, style the audio tag into a touch-friendly player */
 	    {
