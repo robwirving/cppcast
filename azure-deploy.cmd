a38ca8830cf948b6cc594dbd1cf649747bfaaea3
Rob Irving <rob.irving@gmail.com>
2019-11-13 20:32:35 -0500

    Update Hugo version

--- azure-deploy.cmd
+++ azure-deploy.cmd
@@ -7,9 +7,9 @@ if exist bin goto build
 md bin
 cd bin
 :install
-curl -o hugo0563.zip -L https://github.com/gohugoio/hugo/releases/download/v0.56.3/hugo_0.56.3_Windows-64bit.zip
+curl -o hugo0591.zip -L https://github.com/gohugoio/hugo/releases/download/v0.59.1/hugo_0.59.1_Windows-64bit.zip
 echo Installing Hugo...
-SetLocal DisableDelayedExpansion & d:\7zip\7za x hugo0563.zip
+SetLocal DisableDelayedExpansion & d:\7zip\7za x hugo0591.zip
 
 REM Generate Hugo static site from source on GitHub
 :build
