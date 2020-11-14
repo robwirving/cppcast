1ab72768976dbc3b7fc700dad029c2703cc9d25f
Rob Irving <rob.irving@gmail.com>
2020-11-14 11:48:09 -0500

    Site Hugo & Theme update

--- azure-deploy.cmd
+++ azure-deploy.cmd
@@ -7,9 +7,9 @@ if exist bin goto build
 md bin
 cd bin
 :install
-curl -o hugo0591.zip -L https://github.com/gohugoio/hugo/releases/download/v0.59.1/hugo_0.59.1_Windows-64bit.zip
+curl -o hugo0782.zip -L https://github.com/gohugoio/hugo/releases/download/v0.78.2/hugo_0.78.2_Windows-64bit.zip
 echo Installing Hugo...
-SetLocal DisableDelayedExpansion & d:\7zip\7za x hugo0591.zip
+SetLocal DisableDelayedExpansion & d:\7zip\7za x hugo0782.zip
 
 REM Generate Hugo static site from source on GitHub
 :build
